4e34a62bec2db93a61df0b570a96ad219bb39dce
immanuelpotter <manny.potter@capgemini.com>
2017-07-20 08:55:51 +0000

    Hot pink baby

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 @app.route("/")
 def generate_buzz():
 	page = '<html><head>DevOps Buzz Generator</head>'
-	page += '<style>body{background-color: #c43f11}</style><body><h1>'
+	page += '<style>body{background-color: #e42d13}</style><body><h1>'
 	page += generator.generate_buzz()
 	page += '</h1></body></html>'
 	return page
